b66728be8e42d400a76b8022e6bb2560999747d0
gabrielraimondo <gabryfrenk@yahoo.it>
2024-03-06 17:19:29 +0100

    inseriti trending/trending movie

---
+++ js/api/trendingGetters.js
@@ -0,0 +1,38 @@
+const url = 'https://api.themoviedb.org/3/trending/all/day?language=en-US';
+const url2 = 'https://api.themoviedb.org/3/trending/movie/day?language=en-US';
+
+
+const opts = {
+    headers: {
+        "Content-type": "application/json",
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmMjVhYmJjNDY3ZWY2MGQ0OTRiMjYzZmVmYTQ3MGE5YSIsInN1YiI6IjY1ZTdhM2YyNTFmOTlhMDE2MmZiYzJlMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EsFoOSQrXGooVUQiQq4j02cYOcDKkWGu8fXqQcA9PI0"
+    },
+}
+
+export const getTrending = async (baseUrl) => {
+
+    const response = await fetch(baseUrl + 'all/day?language=en-US', opts);
+
+    const data = await response.json();
+
+    return data;
+}
+
+
+/** getTrendingMovies 
+ *
+ * @param baseUrl
+ * @return film  di tendenza
+ 
+getTrending();
+*/ 
+
+export const getTrendingMovie = async (baseUrl) => {
+
+    const response = await fetch(baseUrl + 'movie/day?language=en-US', opts);
+
+    const data = await response.json();
+
+    return data;
+}
+
---
+++ js/index.js
@@ -0,0 +1,6 @@
+import { getTrending, getTrendingMovie } from "./api/trendingGetters.js";
+import {baseUrl} from "./api/config.js";
+
+getTrending(baseUrl);
+getTrendingMovie(baseUrl);
+
